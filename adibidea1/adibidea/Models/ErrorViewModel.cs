1709aef6291d964bbef67b767fe0f8c4b814ab84
salgado.markel <salgado.markel@uni.eus>
2022-11-21 13:56:54 +0100

    adibidea bukatuta

---
+++ adibidea1/adibidea/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace adibidea.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
